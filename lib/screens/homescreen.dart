ee14747f64efec189ec39b31e8173c2d7804b4e7
jeffrgh <jeffrgh@pm.me>
2021-12-30 11:12:54 +0530

    Editited a bit more

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -13,10 +13,10 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       backgroundColor: const Color.fromARGB(15, 237, 237, 237),
       body: Column(
-        children: const [
-          Padding(
+        children: [
+          const Padding(
             padding: EdgeInsets.only(
-              top: 17.5,
+              top: 19.5,
             ),
             child: Center(
               child: Text(
@@ -30,7 +30,32 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
             ),
           ),
-          Divider(),
+          const Divider(
+            thickness: 0.5,
+            color: Colors.white,
+            indent: 50,
+            endIndent: 50,
+          ),
+          const Center(
+            child: Text(
+              'The game of luck',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Image(
+            image: const NetworkImage(
+              'https://media.discordapp.net/attachments/912671770341949470/925739440071135242/Asset_64x.png?width=600&height=677',
+              scale: 1,
+            ),
+            fit: BoxFit.scaleDown,
+            width: MediaQuery.of(context).size.width,
+            height: 350,
+          ),
         ],
       ),
     );
