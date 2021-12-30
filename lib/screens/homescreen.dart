5a68c23f81a374f42039b8c0168e87b36198c560
jeffrgh <jeffrgh@pm.me>
2021-12-30 14:19:42 +0530

    Improved the homepage

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -11,20 +13,20 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(15, 237, 237, 237),
+      backgroundColor: const Color.fromARGB(30, 237, 237, 237),
       body: Column(
         children: [
-          const Padding(
+          Padding(
             padding: EdgeInsets.only(
-              top: 19.5,
+              top: MediaQuery.of(context).size.height * 0.068,
             ),
-            child: Center(
+            child: const Center(
               child: Text(
                 'Bank Card',
                 style: TextStyle(
                   color: Colors.white,
-                  fontSize: 40,
-                  fontWeight: FontWeight.w300,
+                  fontSize: 50,
+                  fontWeight: FontWeight.w100,
                 ),
                 textAlign: TextAlign.center,
               ),
@@ -33,28 +35,96 @@ class _HomeScreenState extends State<HomeScreen> {
           const Divider(
             thickness: 0.5,
             color: Colors.white,
-            indent: 50,
-            endIndent: 50,
+            indent: 90,
+            endIndent: 90,
           ),
           const Center(
             child: Text(
               'The game of luck',
               style: TextStyle(
                 color: Colors.white,
+                fontSize: 16.25,
+                fontWeight: FontWeight.w100,
               ),
             ),
           ),
           const SizedBox(
             height: 10,
           ),
-          Image(
-            image: const NetworkImage(
-              'https://media.discordapp.net/attachments/912671770341949470/925739440071135242/Asset_64x.png?width=600&height=677',
-              scale: 1,
+          Padding(
+            padding: EdgeInsets.only(
+              top: MediaQuery.of(context).size.height * 0.02,
             ),
-            fit: BoxFit.scaleDown,
-            width: MediaQuery.of(context).size.width,
-            height: 350,
+            child: Image(
+              image: const NetworkImage(
+                'https://media.discordapp.net/attachments/912671770341949470/925739440071135242/Asset_64x.png?width=600&height=677',
+                scale: 1,
+              ),
+              fit: BoxFit.scaleDown,
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height * 0.48,
+            ),
+          ),
+          SizedBox(
+            child: RaisedButton(
+              onPressed: () {},
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(
+                  15,
+                ),
+                side: BorderSide(
+                  color: Colors.blue.shade400,
+                  width: 2,
+                ),
+              ),
+              color: Colors.blue.shade300,
+              child: Text(
+                'SINGLE PLAYER',
+                style: TextStyle(
+                  fontSize: 13,
+                  color: Colors.grey.shade900,
+                ),
+              ),
+            ),
+            width: MediaQuery.of(context).size.width * 0.4,
+            height: MediaQuery.of(context).size.height * 0.065,
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.03,
+          ),
+          SizedBox(
+            child: RaisedButton(
+              onPressed: () {},
+              shape: RoundedRectangleBorder(
+                side: BorderSide(
+                  color: Colors.grey.shade900,
+                  width: 2,
+                ),
+                borderRadius: BorderRadius.circular(
+                  15,
+                ),
+              ),
+              color: Colors.transparent,
+              highlightColor: Colors.grey.shade900,
+              child: Text(
+                'MULTI PLAYER',
+                style: TextStyle(
+                  color: Colors.grey.shade600,
+                  fontSize: 13,
+                ),
+              ),
+            ),
+            width: MediaQuery.of(context).size.width * 0.4,
+            height: MediaQuery.of(context).size.height * 0.072,
+          ),
+          Align(
+            child: IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.settings,
+              ),
+            ),
+            alignment: Alignment.bottomLeft,
           ),
         ],
       ),
