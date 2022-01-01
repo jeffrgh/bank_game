a69e4a68c6e220e6203b3f9ca445e7a4c9c82abc
jeffrgh <jeffrgh@pm.me>
2022-01-01 17:08:38 +0530

    added create room screen

---
+++ lib/screens/createroomscreen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class CreateRoomScreen extends StatefulWidget {
+  const CreateRoomScreen({Key? key}) : super(key: key);
+
+  @override
+  _CreateRoomScreenState createState() => _CreateRoomScreenState();
+}
+
+class _CreateRoomScreenState extends State<CreateRoomScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: const [],
+      ),
+    );
+  }
+}
