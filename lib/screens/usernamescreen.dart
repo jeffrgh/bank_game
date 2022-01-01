d88d98fff93b75383661ba741441ad145f9eb745
jeffrgh <jeffrgh@pm.me>
2022-01-01 16:51:26 +0530

    Updated Username screen

--- lib/screens/usernamescreen.dart
+++ lib/screens/usernamescreen.dart
@@ -1,1 +1,19 @@
 import 'package:flutter/material.dart';
+
+class UserNameScreen extends StatefulWidget {
+  const UserNameScreen({Key? key}) : super(key: key);
+
+  @override
+  UserNameScreenState createState() => UserNameScreenState();
+}
+
+class UserNameScreenState extends State<UserNameScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: const [],
+      ),
+    );
+  }
+}
